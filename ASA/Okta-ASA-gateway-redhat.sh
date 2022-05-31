6b072113ccd54f108104b6463ae625920ea02d89
Shad Lutz <90917381+shad-at-okta@users.noreply.github.com>
2022-05-31 13:07:29 -0600

    New scripts added - ASA GW on Redhad & Windows Svr

---
+++ ASA/Okta-ASA-gateway-redhat.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Scripts provided as examples only, with no warranty expressed or implied, and with no ongoing technical support provided by Okta, Inc.
+
+# Gateway Setup token
+GATEWAY_TOKEN="<replace with your gateway token>"
+# Project Enrollment token, to allow for SSH access to the gateway via ASA
+ENROLLMENT_TOKEN="<replace with your gateway project enrollement token>"
+
+function addScaleftRepo (){
+	echo "Ensure curl is installed"
+
+	sudo yum -y install curl
+
+	echo "Download scaleFT repo key"
+
+	sudo rpm --import https://dist.scaleft.com/pki/scaleft_rpm_key.asc
+
+	echo "Add the ScaleFT repo"
+
+	curl -C - https://pkg.scaleft.com/scaleft_yum.repo | sudo tee /etc/yum.repos.d/scaleft.repo
+
+	echo "Rebuild yum cache"
+	sudo yum makecache
+}
+
+function installSft_gateway() {
+
+	echo "Create gateway enrollment token file"
+
+	sudo mkdir -p /var/lib/sft-gatewayd
+
+	echo "$GATEWAY_TOKEN" | sudo tee /var/lib/sft-gatewayd/setup.token
+
+	echo "Install sft-gateway"
+
+	sudo yum -y install scaleft-gateway
+	
+}
+
+function installSftd() {
+
+	echo "Create server agent enrollment token file"
+
+	sudo mkdir -p /var/lib/sftd
+
+	echo "$ENROLLMENT_TOKEN" | sudo tee /var/lib/sftd/enrollment.token
+
+	echo "Add a basic sftd configuration"
+
+	sudo mkdir -p /etc/sft/
+
+	sftcfg=$(cat <<-EOF
+
+	---
+
+	# CanonicalName: Specifies the name clients should use/see when connecting to this host.
+
+	CanonicalName: "$HOSTNAME"
+
+	EOF
+
+	)
+
+	echo -e "$sftcfg" | sudo tee /etc/sft/sftd.yaml
+
+	echo "Install sftd"
+
+	sudo yum -y install scaleft-server-tools
+
+}
+
+function installSftClientTools() {
+	sudo yum -y install scaleft-client-tools 
+}
+
+FLAG="/var/log/firstboot.flag"
+if [ ! -f $FLAG ];then
+
+	echo "This is the first boot"
+
+	#the next line creates an empty flag file so the script won't run the next boot
+	touch $FLAG
+
+    addScaleftRepo
+	installSft_gateway
+	installSftd
+    installSftClientTools
+
+fi
--- ASA/Okta-ASA-server-Windows.cmd
+++ ASA/Okta-ASA-server-Windows.cmd
@@ -7,9 +7,9 @@ echo CanonicalName: %canonical_name% > C:\Windows\System32\config\systemprofile\
 echo Created sftd.yaml file >> C:\sftd_bootstrap.log
 
 echo %enrollment_token% > C:\windows\system32\config\systemprofile\AppData\Local\ScaleFT\enrollment.token
-echo( >> C:\sftd_bootstrap.log
+echo. >> C:\sftd_bootstrap.log
 echo Added Enrollment Token to agent. >> C:\sftd_bootstrap.log
 
 msiexec /qn -i https://dist.scaleft.com/server-tools/windows/latest/ScaleFT-Server-Tools-latest.msi >> C:\sftd_bootstrap.log 2>&1
-echo( >> C:\sftd_bootstrap.log
+echo. >> C:\sftd_bootstrap.log
 echo Installed ASA Server Agent. >> C:\sftd_bootstrap.log
