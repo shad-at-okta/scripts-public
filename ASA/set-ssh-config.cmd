a109a142a74d4210d4293634ef586dbdd19f285e
Shad Lutz <90917381+shad-at-okta@users.noreply.github.com>
2022-12-14 09:41:17 -0700

    Update set-ssh-config.cmd
    Minor syntax changes.

--- ASA/set-ssh-config.cmd
+++ ASA/set-ssh-config.cmd
@@ -4,10 +4,10 @@
 
 @echo off
 #create the .ssh directory if it does not exist.
-mkdir %USERPROFILE%\.ssh 2> nul
+mkdir "%USERPROFILE%\.ssh" 2> nul
 
-echo # This match stanza allows SSH to leverage sft.exe for server name resolution and authentication.  >> %USERPROFILE%\.ssh\config
-echo Match exec "%USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe resolve -q  %%h" >> %USERPROFILE%\.ssh\config
-echo     ProxyCommand %USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe proxycommand  %%h >> %USERPROFILE%\.ssh\config
-echo     UserKnownHostsFile %USERPROFILE%\AppData\Local\ScaleFT\proxycommand_known_hosts >> %USERPROFILE%\.ssh\config
+echo # This Match stanza allows SSH to leverage sft.exe for server name resolution and authentication.  >> "%USERPROFILE%\.ssh\config"
+echo Match exec "%USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe resolve -q  %%h" >> "%USERPROFILE%\.ssh\config"
+echo     ProxyCommand %USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe proxycommand  %%h >> "%USERPROFILE%\.ssh\config"
+echo     UserKnownHostsFile %USERPROFILE%\AppData\Local\ScaleFT\proxycommand_known_hosts >> "%USERPROFILE%\.ssh\config"
 
