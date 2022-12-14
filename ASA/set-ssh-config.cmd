aa3242ee3d56816544bd9acb34463770c6a3debc
Shad Lutz <90917381+shad-at-okta@users.noreply.github.com>
2022-12-14 11:33:59 -0700

    Update set-ssh-config.cmd
    Added support for system-wide installation and 8.3 file paths.

--- ASA/set-ssh-config.cmd
+++ ASA/set-ssh-config.cmd
@@ -8,9 +8,28 @@
 :: create the .ssh directory if it does not exist.
 mkdir "%USERPROFILE%\.ssh" 2> nul
 
+::get 8.8 name for userprofile
+for %%i in (%USERPROFILE%) do set USERPROFILE-SHORT=%%~si
+
+::get 8.3 name for ScaleFT Client Tootls if installed
+if exist "C:\Program Files (x86)\ScaleFT\bin\sft.exe" (
+	for %%i in ("C:\Program Files (x86)\ScaleFT\bin\sft.exe") do set SFT-Path=%%~si
+	Echo Found system-wide installation of ScaleFT Client Tools.
+) else (
+	if exist "%USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe" (
+		for %%i in (%USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe) do set SFT-Path=%%~si
+		Echo Found user specific installation of ScaleFT Client Tools.
+	) else (
+		Echo ScaleFT Client Tools is not installed.  Download them from 
+		Echo https://dist.scaleft.com/client-tools/windows/latest/ 
+		Echo and run this script again.
+	)
+)
+
+if defined SFT-Path (
 :: write Match stanza to .ssh/config
 echo # This Match stanza allows SSH to leverage sft.exe for server name resolution and authentication.  >> "%USERPROFILE%\.ssh\config"
-echo Match exec "%USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe resolve -q  %%h" >> "%USERPROFILE%\.ssh\config"
-echo     ProxyCommand %USERPROFILE%\AppData\Local\Apps\ScaleFT\bin\sft.exe proxycommand  %%h >> "%USERPROFILE%\.ssh\config"
-echo     UserKnownHostsFile %USERPROFILE%\AppData\Local\ScaleFT\proxycommand_known_hosts >> "%USERPROFILE%\.ssh\config"
-
+echo Match exec "%SFT-path" resolve -q  %%h" >> "%USERPROFILE%\.ssh\config"
+echo     ProxyCommand %SFT-Path% proxycommand  %%h >> "%USERPROFILE%\.ssh\config"
+echo     UserKnownHostsFile %USERPROFILE-SHORT%\AppData\Local\ScaleFT\proxycommand_known_hosts >> "%USERPROFILE%\.ssh\config"
+)
